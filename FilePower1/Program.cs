5181921760054d6163f7cb6fb1f6857d016a3779
Tomás Real <tomasreal.art3d@gmail.com>
2025-05-07 15:29:06 +0100

    Add project FilePower1

---
+++ FilePower1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+namespace FilePower1
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string text = "";
+            Queue<string> allText = new Queue<string>();
+            do
+            {
+                text = Console.ReadLine();
+                allText.Enqueue(text);
+            }while(text != "");
+            File.WriteAllLines(args[0], allText);
+
+        }
+    }
+}
