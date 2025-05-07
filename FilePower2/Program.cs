81eb29aaed2fddb0f9ccd912b9d31a9af695fdae
Tomás Real <tomasreal.art3d@gmail.com>
2025-05-07 15:35:45 +0100

    Add project FilePower2

---
+++ FilePower2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+namespace FilePower2
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string text = "";
+            using StreamWriter sw = new StreamWriter (args[0])  ;
+            do
+            {
+                
+                text = Console.ReadLine();
+                if(text!="")
+                    sw.WriteLine(text);
+
+            }while(text != "");
+
+
+        }
+    }
+}
