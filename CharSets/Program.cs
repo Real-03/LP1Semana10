a59bcedf351ebca0650987afda4af882a0d7c00a
Tomás Real <tomasreal.art3d@gmail.com>
2025-05-09 14:51:15 +0100

    Add project CharSets

---
+++ CharSets/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+namespace CharSets
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+            HashSet<char> chars = new HashSet<char>();
+            foreach(string file in args)
+            {
+                using StreamReader sr = new StreamReader(file);
+                
+                while (sr.Peek() >= 0)
+                {
+                    chars.Add(Convert.ToChar(sr.ReadLine()));
+                }
+            }
+
+            Console.WriteLine(string.Join("\n",chars));
+        }
+    }
+}
